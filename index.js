9dde5b51f2f96d8d5686edd7aa68eab1fe138af5
berabotsmd <berahiggins@gmail.com>
2024-08-18 22:31:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//base by DGXeon (Xeon Bot Inc.)
+//base by BRUCE BERA (Xeon Bot Inc.)
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +923257361764
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//YouTube: @BERA_TECH
+//Instagram: BERA-TECH 
+//Telegram: t.me/Alphatrex 
+//GitHub: @berabotsmd
+//WhatsApp: +254743982206
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@BERA_TECH
 
 const {
    spawn
