e9aee889f741d23ee709e3b5fac13a74ad3aef29
berabotsmd <berahiggins@gmail.com>
2024-08-18 22:34:10 +0300

    Update XeonBug6.js

--- XeonBug6.js
+++ XeonBug6.js
@@ -1,11 +1,11 @@
-//base by DGXeon (Xeon Bot Inc.)
+//base by BRUCE BERA (Xeon Bot Inc.)
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//YouTube: @BERA_TECH
+//Instagram: BERA-TECH 
+//Telegram: t.me/Alphatrex 
+//GitHub: @berabotsmd
+//WhatsApp: +254743982206
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@BERA_TECH
 
 const { default: makeWaSocket, useMultiFileAuthState, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@whiskeysockets/baileys')
 const os = require('os')
